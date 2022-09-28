b8f9b16427520b74d8c0c7e8c7edc544ee882380
DESKTOP-HOVGRGR\gkdrk <gkdrk99@naver.com>
2022-09-29 08:44:25 +0900

    220929 데이터 검증-1까지

--- src/main/java/com/dendtal/api/controller/PostController.java
+++ src/main/java/com/dendtal/api/controller/PostController.java
@@ -2,8 +2,13 @@ package com.dendtal.api.controller;
 
 import com.dendtal.api.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -14,9 +19,21 @@ public class PostController {
     @PostMapping ("/posts")
     //public String post(@RequestParam String title, @RequestParam String content){
     //public String post(@RequestParam Map<String,String> params){
-    public String post(@RequestBody PostCreate params){  // ModelAttribute는 생략가능
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {  // ModelAttribute는 생략가능
         log.info("params={}",params.toString());
-        return "Hello World";
+
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField(); // title
+            String errorMessage = firstFieldError.getDefaultMessage(); // ...에러 메시지
+
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
+
+        return Map.of();
     }
 
 
--- src/main/java/com/dendtal/api/request/PostCreate.java
+++ src/main/java/com/dendtal/api/request/PostCreate.java
@@ -1,18 +1,22 @@
 package com.dendtal.api.request;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
 @ToString
 public class PostCreate {
 
+    @NotBlank(message = "타이틀을 입력해주세요")
     public String title;
+
+    @NotBlank(message = "컨텐츠를 입력해주세요")
     public String content;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+
 }
